5ab89cf905ceade07aa7bc02bf6fb29135404efe
Alex Buly <asbuly@icloud.com>
2024-09-15 16:47:00 -0400

    Deletes old data and adds formatting

--- script.js
+++ script.js
@@ -2,24 +2,64 @@
 function data() {
     const cityInput = document.querySelector("#city");
     const citySubmit = document.querySelector("#city-submit");
+    const dataContainer = document.querySelector(".data-container");
+    const name = document.querySelector(".cityName");
+
     async function fetchWeather(cityName) {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?unitGroup=us&elements=datetime%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Chumidity%2Cprecip%2Ccloudcover&include=days%2Ccurrent&key=8A45R9EG9FAGY786X8WCEGF8M&contentType=json`, {mode: 'cors'});
         const data = await response.json();
         console.log(data);
-    
+
+        let inputs;
+        let arrDays = [];
+        inputs = document.createElement("div");
+        inputs.classList.add("temps");
+        dataContainer.appendChild(inputs);
         // Narrow down to access specific attributes 
         for (let i = 0; i < data.days.length; i++)  {
             const tempMax = data.days[i].tempmax;
             const tempMin = data.days[i].tempmin;
-            console.log(` day ${i +1} max ` + tempMax);
-            console.log(`day ${i + 1} min ` + tempMin);
+            arrDays = [];
+            const dayTitle = document.createElement("div");
+            dayTitle.classList.add("day-title");
+            const days = document.createElement("div");
+            days.classList.add("inputDays");
+            inputs.appendChild(dayTitle);
+            inputs.appendChild(days);
+            dayTitle.textContent = `Day ${i +1 }: `
+    
+            // inputs = document.createElement("div");
+            // inputs.classList.add("temps");
+            // dataContainer.appendChild(inputs);
+            //inputs.textContent = '';
+            arrDays.push(tempMax);
+            arrDays.push(tempMin);
+            console.log(arrDays);
+            console.log(`Day ${i + 1} max: ` + tempMax);
+            console.log(`Day ${i + 1} min: ` + tempMin);
+            console.log(name);
+
+            // Display to page 
+            days.textContent = ` Max: ${tempMax} Min: ${tempMin}`;
+            name.textContent = data.address;
+            //location.textContent = data
             //if (clouds < 7 ) console.log("Value " + clouds);
         }
     }
 
+    const deleteChild = () => {
+        let child = dataContainer.lastElementChild;
+        while (child) {
+            dataContainer.removeChild(child);
+            child = dataContainer.lastElementChild;
+        }
+    }
+
+    // Form to fetch data 
     citySubmit.addEventListener("click", () => {
         const searchTerm = cityInput.value.trim();
         if (searchTerm) {
+            deleteChild();
             fetchWeather(searchTerm);
         }
     })
@@ -42,11 +82,5 @@ function data() {
 //fetchWeather();
 
 const myData = data();
-
-// Form to fetch data 
-
-
-// Display to page 
-
 
 

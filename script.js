19a5231cb2345069971542bc1b3e42483e2d9165
Alex Buly <asbuly@icloud.com>
2024-09-14 21:24:59 -0400

    Returns city based on user input

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // Fetch from API
 function data() {
-    async function fetchWeather() {
-        const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Huntington?unitGroup=us&elements=datetime%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Chumidity%2Cprecip%2Ccloudcover&include=days%2Ccurrent&key=8A45R9EG9FAGY786X8WCEGF8M&contentType=json');
+    const cityInput = document.querySelector("#city");
+    const citySubmit = document.querySelector("#city-submit");
+    async function fetchWeather(cityName) {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?unitGroup=us&elements=datetime%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Chumidity%2Cprecip%2Ccloudcover&include=days%2Ccurrent&key=8A45R9EG9FAGY786X8WCEGF8M&contentType=json`, {mode: 'cors'});
         const data = await response.json();
         console.log(data);
     
@@ -14,6 +16,15 @@ function data() {
             //if (clouds < 7 ) console.log("Value " + clouds);
         }
     }
+
+    citySubmit.addEventListener("click", () => {
+        const searchTerm = cityInput.value.trim();
+        if (searchTerm) {
+            fetchWeather(searchTerm);
+        }
+    })
+
+
     return {fetchWeather} 
 }
 // async function fetchWeather() {
@@ -31,8 +42,9 @@ function data() {
 //fetchWeather();
 
 const myData = data();
-myData.fetchWeather();
+
 // Form to fetch data 
+
 
 // Display to page 
 
